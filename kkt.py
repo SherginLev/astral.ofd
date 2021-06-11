e998ade3ba3b096ce7bdb6b9020c4f868addc074
SherginLev <work.mail.lev@gmail.com>
2021-06-11 16:04:34 +0900

    Добавление новых полей в config.xml, создал два класса для касс и торговых точек, не уверен, что они понадобятся

---
+++ kkt.py
@@ -0,0 +1,6 @@
+class Kkt():
+    def __init__(self, kkt_num, kkt_name):
+        self.kkt_num = kkt_num
+        self.kkt_name = kkt_name
+
+    kkt_ofd_id = ''
---
+++ store.py
@@ -0,0 +1,5 @@
+class Store():
+    def __init__(self, id, alias, number_cash):
+        self.id = id
+        self.alias = alias
+        self.number_cash = number_cash
